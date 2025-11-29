91930c4ca33bd479147e430c13f68186025041e2
justine-wee <justine_wee@dlsu.edu.ph>
2025-11-29 15:37:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -69,7 +69,9 @@ app.post('/api/delete', async (req, res) => {
 
 app.get('/api/database', async (req, res) => {
     try {
-        const result = await db.selectQuery("", "LIMIT 20", 1900, 2100, 1);
+        const result = await db.selectQuery("", "ORDER BY tconst ASC LIMIT 10", 1900, 2100, 1);
+        console.log("DB Result Preview:", result[0]);
+        
         if (result) {
             res.status(200).json(result); 
         } else {
