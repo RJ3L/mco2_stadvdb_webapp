b5aa3fc348170ba63240a5072c4e558d3837fd83
Richard Limtin <jeremylimtin@gmail.com>
2025-11-29 23:39:00 +0800

    pingNode

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const db = require("./models/db.js");
-const syncUtils = require("./models/sync.js");
+const db = require("./models/db.js")
+const syncUtils = require("./models/sync.js")
+const {nodeUtils} = require('./models/nodes.js');
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -83,6 +84,64 @@ app.get('/api/database', async (req, res) => {
     }
 });
 
+app.get('/api/pingNode/:nodeNum', async (req, res) => {
+    const nodeNum = parseInt(req.params.nodeNum);
+    try {
+        console.log(`Pinging Node ${nodeNum}...`);
+        let isAlive = false;    
+        if (nodeNum === 1) {
+            isAlive = await nodeUtils.pingNode(1);
+        }
+        else if (nodeNum === 2) {
+            isAlive = await nodeUtils.pingNode(2);
+        }
+        else if (nodeNum === 3) {
+            isAlive = await nodeUtils.pingNode(3);
+        } else {
+            return res.status(400).json({ message: 'Invalid node number' });
+        }   
+        res.status(200).json({ node: nodeNum, alive: isAlive });
+    } catch (error) {
+        res.status(500).json({ message: 'Ping failed', error: error.message });
+    }
+
+});
+
+
+/* 
+    Query: WHERE = ...
+    Limit: LIMIT ...
+*/
+async function testSelect(query, limit, from, to, node){
+    let res = await db.selectQuery(query, limit, from, to, node)
+    console.log("Result: " + JSON.stringify(res))
+}
+
+/*
+    Query: VALUES(...)
+*/
+async function testInsert(query, startYear, node){
+    let res = await db.insertQuery(query, startYear, node)
+    console.log("Result: " + res)
+}
+
+async function testDelete(query, startYear, node){
+    let res = await db.deleteQuery(query, startYear, node)
+    console.log("Result: " + res)
+}
+
+async function testMasterSync(){
+    let res = await syncUtils.syncMaster()
+    console.log(res)
+}
+async function testFragSync(node){
+    let res = await syncUtils.syncFragment(node)
+    console.log(res)
+}
+async function testUpdate(valuesQuery, tconst, year, node){
+    let res = await db.updateQuery(valuesQuery, tconst, year, node)
+    console.log("Result: " + res)
+}
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
